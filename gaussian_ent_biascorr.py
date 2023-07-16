5603cf30769314dd176199adf1bd745f736cc599
Guillaume Girier <55884546+GuillaumeGirier@users.noreply.github.com>
2023-07-16 22:26:37 +0200

    Update gaussian_ent_biascorr.py

--- gaussian_ent_biascorr.py
+++ gaussian_ent_biascorr.py
@@ -6,7 +6,7 @@
 
 COMMENTS :
 
-If you want to test this code, you can find a toy at the end of the script.
+If you want to test this code, you can find a toy in the Jupyter Notebook called "High_order_notebook.ipynb".
 If you want more information, refers to "high_order.py" comments.
 
 """
