ba9e6c8c2f137a13b957eeb801e638f80530cd49
Guillaume Girier <55884546+GuillaumeGirier@users.noreply.github.com>
2023-07-16 22:27:27 +0200

    Update soinfo_from_covmat.py

--- soinfo_from_covmat.py
+++ soinfo_from_covmat.py
@@ -7,7 +7,7 @@
 COMMENTS :
 
 This script code is used by "high_order.py" and use "gaussian_ent_biascorr.py" script.
-If you want to test this code, you can find a toy at the end of the script.
+If you want to test this code, you can find a toy in the Jupyter Notebook called "High_order_notebook.ipynb".
 If you want more information, refers to "high_order.py" comments.
 
 """
