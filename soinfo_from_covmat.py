99ab7c3cbe637941c50055000986bdbf78f4e398
Guillaume Girier <55884546+GuillaumeGirier@users.noreply.github.com>
2023-07-17 21:37:58 +0200

    Update soinfo_from_covmat.py

--- soinfo_from_covmat.py
+++ soinfo_from_covmat.py
@@ -4,6 +4,14 @@
 @email : guillaumegirier@gmail.com
 @commun_work : GIRIER Guillaume, DESROCHES Mathieu, RODRIGUEZ Serafim
 
+LICENSE :
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 3
+
 COMMENTS :
 
 This script code is used by "high_order.py" and use "gaussian_ent_biascorr.py" script.
